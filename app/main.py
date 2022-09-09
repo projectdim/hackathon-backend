ecfc01173ea7a6be2e39c3fd4d27c3d3d180bb34
Michał Bargiel <m.bargiel.ctr@1010data.com>
2022-09-09 13:38:50 +0200

    Add host to start command.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(app="app.app:app", reload=True, port=5000)
+    uvicorn.run(
+        app="app.app:app",
+        host="0.0.0.0",
+        reload=True,
+        port=5000,
+    )
