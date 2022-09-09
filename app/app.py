ad32828f62b181df328ee5227a98558982d24444
Michał Bargiel <m.bargiel.ctr@1010data.com>
2022-09-09 17:41:34 +0200

    Add cors.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
 
 from app.routers.markers import router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 app.mount("/static", StaticFiles(directory="pictures"), name="static")
-
 
 @app.get("/")
 def hello():
