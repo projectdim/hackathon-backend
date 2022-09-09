39cb654521369ef57ed3b8e847e55d469b28f718
Michał Bargiel <m.bargiel.ctr@1010data.com>
2022-09-09 22:07:10 +0200

    Wait for db.

--- app/support/database/connection.py
+++ app/support/database/connection.py
@@ -1,21 +1,35 @@
-from config.local import config
+import logging
+from time import sleep
+
 import psycopg2
 
+from config.local import config
+
 schema = config['database']
+log = logging.getLogger(__name__)
+tries = 0
+max_tries = 5
+sleep_time = 5
 
-try:
-    # conn = psycopg2.connect("dbname='dim_db' user='postgres' host='db' password='password'")
-    print(config)
+while tries < 5:
+    try:
+        # conn = psycopg2.connect("dbname='dim_db' user='postgres' host='db' password='password'")
+        conn = psycopg2.connect(user=config['user'],
+                                password=config['password'],
+                                host=config['host'],
+                                port=config['port'],
+                                database=config['database']
+                                )
 
-    conn = psycopg2.connect(user=config['user'],
-                            password=config['password'],
-                            host=config['host'],
-                            port=config['port'],
-                            database=config['database']
-                            )
+        cur = conn.cursor()
+    except OSError as err:
+        print(err)
+    except Exception as e:
+        print(e)
+        print("Unable to connect to the database")
+    else:
+        break
 
-    cur = conn.cursor()
-except OSError as err:
-    print(err)
-except:
-    print("Unable to connect to the database")
+    log.error("Failed, sleeping...")
+    tries += 1
+    sleep(sleep_time)
