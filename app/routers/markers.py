6afd352d2076e4c4e5267d529e95908744b8b948
marta <marta.gierula.ctr@1010data.com>
2022-09-09 20:05:16 +0200

    schemas structure reorg

--- app/routers/markers.py
+++ app/routers/markers.py
@@ -2,17 +2,9 @@ import datetime
 from typing import List
 
 from fastapi import APIRouter
-from pydantic import BaseModel
-
-from app.routers.schemas import Marker, MarkerCreate
+from app.routers.schemas import Marker, MarkerCreate, Pin
 
 router = APIRouter(prefix="/markers")
-
-
-class Pin(BaseModel):
-    id: str
-    lat: float
-    lng: float
 
 
 @router.get("/", response_model=List[Pin])
@@ -30,28 +22,20 @@ def get_all_markers():
 def marker_detail(marker_id: str):
     return {
         "id": "tnehosu",
-        "lat": 54.55,
-        "lng": 22.15,
         "address": {
             "city": "Krakow",
             "zipcode": "00-001",
             "street": "Krupnicza"
         },
         "photos": [{
-            "id": "a",
-            "label": "a person",
             "url": "http://localhost:8000/static/test_1.jpg"
         }, {
-            "id": "b",
-            "label": "an other person",
             "url": "http://localhost:8000/static/test_2.jpg"
         }],
         "statuses": [{
-            "id": "whatever",
             "type": "building",
             "statusKey": "stable",
             "safetyLevel": 3,
-            "distance": "whatever",
             "modifiedDate": datetime.datetime.utcnow(),
             "description": "b"
         }]
--- app/routers/schemas.py
+++ app/routers/schemas.py
@@ -1,5 +1,18 @@
+import datetime
+from typing import Literal
+from pydantic import Field
+
 from pydantic import BaseModel
-from app.routers.create_schemas import MarkerCreate, PhotoCreate, StatusCreate
+
+
+class Pin(BaseModel):
+    id: str
+    lat: float
+    lng: float
+
+
+class Data(BaseModel):
+    markers: list["Pin"]
 
 
 class Address(BaseModel):
@@ -8,12 +21,41 @@ class Address(BaseModel):
     street: str
 
 
-class Photo(PhotoCreate):
-    id: str
+class Photo(BaseModel):
+    url: str
 
 
-class Status(StatusCreate):
-    id: str
+class Status(BaseModel):
+    type: Literal[
+        'building',
+        'electricity',
+        'water',
+        'roads',
+        'fuel'
+    ]
+    status_key: Literal[
+        'intact',
+        'stable',
+        'accessible',
+        'closed',
+        'open'
+    ] = Field(alias='statusKey')
+
+    safety_level: bool = Field(alias='safetyLevel')
+    modified_date: datetime.datetime = Field(alias='modifiedDate')
+    description: str
+
+
+class Contact(BaseModel):
+    label: str
+    telephone_number: float = Field(alias='telephoneNumber')
+    email: str
+
+
+class MarkerCreate(BaseModel):
+    address: "Address"
+    photos: list["Photo"]
+    statuses: list["Status"]
 
 
 class Marker(MarkerCreate):
